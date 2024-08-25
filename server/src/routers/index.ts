20980eab1c48bbeb768011f7d976742e7a91c219
oleksii-sdrk <oleksii.fsdev@gmail.com>
2024-08-26 00:45:22 +0200

    added authorization and /user/current endpoint

--- server/src/routers/index.ts
+++ server/src/routers/index.ts
@@ -4,12 +4,14 @@ import { authRouter } from './auth.ts';
 import { statusHandler } from '../middlewares/statusHandler.ts';
 import { notFoundHandler } from '../middlewares/notFoundHandler.ts';
 import { errorHandler } from '../middlewares/errorHandler.ts';
+import { userRouter } from './user.ts';
 
 export const router = Router();
 
 router.get('/', statusHandler);
 
 router.use('/auth', authRouter);
+router.use('/user', userRouter);
 
 router.use('/*', notFoundHandler);
 router.use(errorHandler);
