7237a52e036ba93fe90b02127ac3e03dd720c50e
dmvoytyuk <dmvoytyuk@gmail.com>
2024-08-25 13:01:25 +0200

    home page init

--- client/src/pages/HomePage/HomePage.tsx
+++ client/src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,10 @@
 import HomePageLayout from "../../components/HomePageLayout/HomePageLayout.tsx";
 import Layout from "../../components/Layout/Layout.tsx";
+import bgImage from "../../assets/img/bg/homepagebg.jpeg";
 
 const HomePage = () => {
   return (
-    <Layout>
+    <Layout backgroundURL={bgImage}>
       <HomePageLayout />
     </Layout>
   );
