0b5e84ea595a19f47c9139bdb1f0eb0987122ca2
dmvoytyuk <dmvoytyuk@gmail.com>
2024-08-26 19:32:30 +0200

    SearchPage route

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "../../pages/HomePage/HomePage.tsx";
+import SearchPage from "../../pages/SearchPage/SearchPage.tsx";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/search" element={<SearchPage />} />
     </Routes>
   );
 }
